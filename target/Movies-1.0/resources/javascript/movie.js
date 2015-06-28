90f40a8fffdc62af7b7efd5c6eee4a7b5ca30211
Krzysztof Lamkiewicz <krzysztof.lamkiewicz@gmail.com>
2015-06-28 12:03:33 +0200

    Squashed commits

--- target/Movies-1.0/resources/javascript/movie.js
+++ target/Movies-1.0/resources/javascript/movie.js
@@ -2,7 +2,15 @@ $(function() {
     console.log("working jquery");
     console.log(someValue);
     //pstr
-    $.get( "http://www.omdbapi.com/?t="+someValue.title, function( data ) {
-        console.log(data.Plot);
+   // $.get( "http://www.omdbapi.com/?t="+someValue.title, function( data ) {
+   //     console.log(data.Plot);
+   // });
+
+    $.ajax({
+	url: "//www.omdbapi.com/?t="+someValue.title,
+	dataType: "jsonp",
+	success: function( data ) {
+        	console.log(data.Plot);
+    	}
     });
 });
